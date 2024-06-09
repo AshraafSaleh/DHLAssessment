e6e75abbcf5a8240b974a302df540b279d36fd5e
Muhammad Ashraaf Saleh <mashraafsaleh@gmail.com>
2024-06-09 19:14:04 +0800

    DHL Assessment

---
+++ DHLAssessment/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DHLAssessment.Models;
+
+namespace DHLAssessment.Controllers
+{
+    public class LoginController : Controller
+    {
+        //Hardcoded credentials
+        private const string HardcodedUsername = "Ashraaf";
+        private const string HardcodedPassword = "Password";
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (model.UserName == HardcodedUsername && model.Password == HardcodedPassword)
+            {
+                return RedirectToAction("Account");
+            }
+            ViewBag.ErrorMessage = "Invalid username or password";
+            return View();
+        }
+
+        public IActionResult Account()
+        {
+            return View();
+        }
+    }
+}
